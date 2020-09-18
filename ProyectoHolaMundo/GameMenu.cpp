51eafd214becd9ace91a3eaaed603c6bd2055079
Luis Flores <luismejia2109@gmail.com>
2020-09-18 00:20:10 -0600

    video

--- ProyectoHolaMundo/GameMenu.cpp
+++ ProyectoHolaMundo/GameMenu.cpp
@@ -8,6 +8,7 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_video.h>
 #include "Rueda.h"
 #include "Preguntas.h"
 #include "Nivel2.h"
@@ -635,7 +636,6 @@ int main()
     al_init();
     al_init_font_addon();
     al_init_ttf_addon();
-
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 5; j++) {
             preguntaRepetida[i][j] = false;
@@ -647,6 +647,7 @@ int main()
 
     ALLEGRO_FONT* font = al_load_ttf_font("YARDSALE.ttf", 64, 0);
     ALLEGRO_FONT* font2 = al_load_ttf_font("YARDSALE.ttf", 36, 0);
+    ALLEGRO_FONT* font3 = al_load_ttf_font("YARDSALE.ttf", 18, 0);
 
     queue = al_create_event_queue();
     must_init(queue, "queue");
@@ -654,8 +655,82 @@ int main()
     must_init(al_init_image_addon(), "filo_background");
 
     must_init(al_install_mouse(), "mouse");
-
+    must_init(al_install_keyboard(), "keyboard");
+    al_register_event_source(queue, al_get_keyboard_event_source());
     must_init(al_init_primitives_addon(), "primitives");
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 25.0);
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    must_init(al_init_video_addon(), "video");
+    ALLEGRO_VIDEO* intro = al_open_video("Intro.ogv");
+    al_register_event_source(queue, al_get_video_event_source(intro));
+    al_start_video(intro, al_get_default_mixer());
+
+    bool redraw = true;
+    bool use_frame_events = false;
+    bool videoEnd = false;
+    //al_set_video_playing(intro, !al_is_video_playing(intro));
+    al_start_timer(timer);
+    ALLEGRO_BITMAP* frame = al_create_bitmap(al_get_video_scaled_width(intro), al_get_video_scaled_height(intro));
+    while (true)
+    {
+
+        frame = al_get_video_frame(intro);
+        if (frame)
+            al_draw_scaled_bitmap(frame, 0, 0, 1280, 720, 0, 0, 800, 450, 0);
+        else
+            cout << "Null";
+
+        al_draw_text(font3, al_map_rgb(255, 255, 255), 0, 0, 0, "Presiona Esc para saltar");
+        al_wait_for_event(queue, &event);
+        switch (event.type) {
+        case ALLEGRO_EVENT_KEY_DOWN:
+            switch (event.keyboard.keycode) {
+            case ALLEGRO_KEY_SPACE:
+                
+                //al_set_video_playing(intro, !al_is_video_playing(intro));
+                break;
+            case ALLEGRO_KEY_ESCAPE:
+                al_close_video(intro);
+                videoEnd = true;
+                break;
+            default:
+                break;
+            }
+            break;
+
+        case ALLEGRO_EVENT_TIMER:
+            /*
+            display_time += 1.0 / 60;
+            if (display_time >= video_time) {
+               video_time = display_time + video_refresh_timer(is);
+            }*/
+            /*if (!use_frame_events) {
+                redraw = true;
+            }*/
+            break;
+
+        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+            al_close_video(intro);
+            break;
+
+        case ALLEGRO_EVENT_VIDEO_FRAME_SHOW:
+            if (use_frame_events) {
+                redraw = true;
+            }
+            break;
+
+        case ALLEGRO_EVENT_VIDEO_FINISHED:
+            videoEnd = true;
+            break;
+        default:
+            break;
+        }
+        al_flip_display();
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+        if (videoEnd)
+            break;
+    }
 
     ALLEGRO_BITMAP* background = al_load_bitmap("castillo1.jpg");
     if (!background)
